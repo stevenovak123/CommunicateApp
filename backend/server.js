0886825c83b432d9ab6cd8cabc1b3fdf7a795fea
stevenovak123 <stevenovak0802@gmail.com>
2022-03-02 21:54:55 +0530

    added backend for video call

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ const userRoutes = require('./routes/userRoutes')
 const chatRoutes = require('./routes/chatRoutes')
 const messageRoutes = require('./routes/messageRoutes')
 const { notFound, errorHandler } = require('./middleware/errorMiddleware')
+const cors = require('cors')
+
 // initialize backend
 const app = express()
 
@@ -23,6 +25,7 @@ app.use('/api/message', messageRoutes)
 app.use(notFound)
 
 app.use(errorHandler)
+app.use(cors())
 
 const PORT = process.env.PORT || 8000
 
@@ -63,5 +66,23 @@ io.on('connection', (socket) => {
 	socket.off('setup', () => {
 		console.log(`User Disconnected`)
 		socket.leave(userData._id)
+	})
+})
+// video calling functionality
+
+io.on('videoConnection', (socket) => {
+	socket.emit('me', socket.id)
+	socket.on('videoDisconnect', () => {
+		socket.broadcast.emit('Call Ended')
+	})
+	socket.on('callUser', ({ userToCall, signalData, from, name }) => {
+		io.to(userToCall).emit('callUser', {
+			signal: signalData,
+			from,
+			name,
+		})
+		socket.on('answerCall', (data) => {
+			io.to(data.to).emit('callaccepted', data.signal)
+		})
 	})
 })
